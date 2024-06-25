dd89cf47bfb17db380f551684959014ef3d50a82
servalabs.com <166895751+servalabs@users.noreply.github.com>
2024-06-25 21:21:44 +0530

    Update SkillIcon.jsx

--- src/components/SkillIcon.jsx
+++ src/components/SkillIcon.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 const SkillIcon = ({ path }) => {
-    return (
-        <div className="flex items-center justify-center w-[10px] md:w-[90px] 2xl:w-[110px] h-[70px] md:h-[90px] 2xl:h-[110px] rectangle rounded-lg bg-[#292929]">
-            <img src={path} className="w-[34px] md:w-[100px] 2xl:w-[200px] h-[60px]" />
-        </div>
-    );
+ return (
+ <div className="flex items-center justify-center w-[80px] md:w-[90px] 2xl:w-[110px] h-[80px] md:h-[90px] 2xl:h-[110px] rectangle rounded-lg bg-[#292929]">
+ <img src={path} className="w-[74px] md:w-[10px] 2xl:w-[200px] h-[60px]" />
+ </div>
+ );
 };
 
 export default SkillIcon;
