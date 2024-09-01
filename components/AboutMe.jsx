c87df2d44a8b29b5492639bfd01d4ddd2a408fbd
Priyanshu Uchat <85685604+ITSMEPRIYANSHUUCHAT@users.noreply.github.com>
2024-09-01 23:44:06 +0530

    Making major phone layout changes
    These are the final phone_layout changes

---
+++ components/AboutMe.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { motion, useTransform, useScroll } from "framer-motion";
+
+import Wrapper from "./Wrapper";
+import flag from "../assets/bharat.png";
+import pe1 from "../assets/sec-2-p-e-1.png";
+import pe2 from "../assets/sec-2-p-e-2.png";
+import Div from "./Div";
+
+const AboutMe = () => {
+    const { scrollY } = useScroll();
+    const y1 = useTransform(scrollY, [0, 500], [100, 0], { clamp: false });
+    const y2 = useTransform(scrollY, [0, 500], [0, -200], { clamp: false });
+    return (
+        <div
+            id="about"
+            className="w-full py-[50px] md:py-[100px] bg-white text-black relative overflow-hidden"
+        >
+            {/* BACKGROUND ELEMENTS START */}
+            <span className="sec-2-bg-gradient" />
+            <motion.img className="sec-2-p-e-1" style={{ y: y1 }} src={pe1} />
+            <motion.img className="sec-2-p-e-2" style={{ y: y2 }} src={pe2} />
+            {/* BACKGROUND ELEMENTS END */}
+
+            <Wrapper>
+                {/* HEADING START */}
+                <Div className="text-[40px] md:text-[90px] 2xl:text-[90px] leading-[52px] md:leading-[95px] 2xl:leading-[123px] font-oswald uppercase mb-[25px] md:mb-[40px] 2xl:mb-[60px] flex flex-col relative">
+                    <span>A Founder Based</span>
+                    <span className="flex items-center gap-2">
+                        <span>In</span>
+                        <img
+                            src={flag}
+                            alt=""
+                            className="w-[70px] md:w-[150px] 2xl:w-[190px] block mt-1 md:mt-2"
+                        />
+                        <span>Bharat</span>
+                    </span>
+                </Div>
+                {/* HEADING END */}
+
+                {/* PARAGRAPH START */}
+                <Div className="max-w-[759px] 2xl:max-w-[959px] text-[16px] 2xl:text-[20px] leading-[24px] 2xl:leading-[32px] mb-[20px] md:mb-[30px] 2xl:mb-[50px] relative">
+                "People often overestimate what they can do in a year and underestimate what they can do in a decade."
+
+                </Div>
+                <Div className="max-w-[759px] 2xl:max-w-[959px] text-[16px] 2xl:text-[20px] leading-[24px] 2xl:leading-[32px] mb-[20px] md:mb-[30px] 2xl:mb-[50px] relative">
+                Started TechGG in 2021 to help brands who are working on the edge, From Tech Hardware companies like Sharge (Powerbank with a display & OS) to mymind (AI-based Personal Knowledge Management System)
+                </Div>
+
+                <Div className="max-w-[759px] 2xl:max-w-[959px] text-[16px] 2xl:text-[20px] leading-[24px] 2xl:leading-[32px] mb-[20px] md:mb-[30px] 2xl:mb-[50px] relative">
+                I like to take as much information as I humanly can and process it via a complex system, perks of being a polymath
+                </Div>
+                {/* PARAGRAPH END */}
+
+                {/* PARAGRAPH START */}
+                <Div className="max-w-[759px] 2xl:max-w-[959px] text-[16px] 2xl:text-[20px] leading-[24px] 2xl:leading-[32px] relative">
+                Jack of all trades, master of none; oftentimes better than master of one
+                </Div>
+                {/* PARAGRAPH END */}
+            </Wrapper>
+        </div>
+    );
+};
+
+export default AboutMe;
---
+++ data.js
@@ -0,0 +1,83 @@
+import p1 from "./assets/scrap.png";
+import p2 from "./assets/arbic.png";
+import p3 from "./assets/dashboard_infographics.png";
+import p4 from "./assets/fyp.png";
+import p5 from "./assets/oman.png";
+import p6 from "./assets/CS.png";
+
+export const filters = [
+    {
+        name: "All",
+        id: "all",
+    },
+    {
+        name: "Dashboard",
+        id: "dashboard",
+    },
+    {
+        name: "Mobile App",
+        id: "mobile-app",
+    },
+    {
+        name: "Website",
+        id: "website",
+    },
+];
+
+export const projects = [
+    {
+        name: "ScrapHub - A digital platform for scrap buyers and sellers",
+        image: p1,
+        tags: ["dashboard", "website"],
+        codeLink: "https://github.com/servalabs",
+        // videoLink: "https://www.youtube.com/",
+    },
+    {
+        name: "Corpus- Manage and analyze text",
+        image: p2,
+        tags: ["dashboard"],
+        codeLink: "https://github.com/servalabs",
+    },
+    {
+        name: "Data-Visulization-Infogrphics",
+        image: p3,
+        tags: ["all"],
+        codeLink: "https://github.com/servalabs",
+    },
+    {
+        name: "FProject- We Are Solving Global Problems",
+        image: p4,
+        tags: ["mobile-app", "dashboard"],
+        codeLink: "https://github.com/servalabs",
+    },
+    {
+        name: "Oman Security system - Manage Daily Security Operations",
+        image: p5,
+        tags: ["website", "dashboard"],
+        codeLink: "https://github.com/servalabs",
+    },
+    {
+        name: "Client Server-JAVA-JAVAFX",
+        image: p6,
+        tags: ["all"],
+        codeLink: "https://github.com/servalabs",
+    },
+];
+
+export const achievements = [
+    {
+        id: 1,
+        year: 32,
+        description: "SUCCESSFUL<br />PROJECTS",
+    },
+    {
+        id: 2,
+        year: 25,
+        description: "HARDWARE PARTNERS",
+    },
+    {
+        id: 3,
+        year: 3,
+        description: "YEARS OF<br />EXPERIENCE ",
+    },
+];
