d4d09d49b2fb968012bf967596fbcefa430a6cee
Priyanshu Uchat <85685604+ITSMEPRIYANSHUUCHAT@users.noreply.github.com>
2024-12-13 13:20:55 +0530

    some fixing

--- src/pages/Timeline.jsx
+++ src/pages/Timeline.jsx
@@ -59,7 +59,7 @@ const Timeline = () => {
   return (
     <div className='main'>
       <div className="flex flex-wrap justify-center gap-2 md:gap-0 md:flex-col text-[40px] md:text-[90px] 2xl:text-[120px] leading-[40px] md:leading-[95px] 2xl:leading-[123px] font-oswald uppercase text-gradient mb-10 md:mb-0 md:sticky top-2">
-        <span>TIMELINE</span>
+        <span>Experience</span>
       </div>
 
       <div className='timeline'>
